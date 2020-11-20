21cf34b6835ab32c8c6b3abcc78ac18ab171beb7
elismountain <echospb@mail.ru>
2020-11-20 15:38:58 +0000

    task 3 fixed

--- src/main.js
+++ src/main.js
@@ -3,33 +3,32 @@ import {createCardsTemplate} from './view/cards-template.js';
 import {createSortTemplate} from './view/sort.js';
 import {createTripInfoTemplate} from './view/trip-info.js';
 import {createTripCostTemplate} from './view/trip-cost.js';
-import {createEditFormTemplate} from './view/edit-form.js';
-import {createCardTemplate} from './view/card.js';
 import {createFilterTemplate} from './view/filter.js';
 import {createMenuTemplate} from './view/menu.js';
-// import {createNewPointTemplate} from './view/new-point.js';
+import {createEditFormTemplate} from './view/edit-form.js';
+import {createCardTemplate} from './view/card.js';
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const tripInfoElement = document.querySelector(`.page-header__logo`);
-render(tripInfoElement, createTripInfoTemplate(), `afterend`);
+const tripInfoElement = document.querySelector(`.trip-main`);
+render(tripInfoElement, createTripInfoTemplate(), `afterbegin`);
 
-const tripCostElement = document.querySelector(`.trip-main`);
-render(tripCostElement, createTripCostTemplate(), `beforeend`);
+const tripCostElement = document.querySelector(`.trip-info__main`);
+render(tripCostElement, createTripCostTemplate(), `afterend`);
 
 
 const tripControlsElement = document.querySelector(`.trip-main__trip-controls`);
-render(tripControlsElement, createMenuTemplate(), `beforeend`);
-
 const tripControlsMenuElement = tripControlsElement.querySelector(`h2`);
-render(tripControlsMenuElement, createFilterTemplate(), `afterend`);
+
+render(tripControlsElement, createFilterTemplate());
+render(tripControlsMenuElement, createMenuTemplate(), `afterend`);
 
 
 const tripEventsElement = document.querySelector(`.trip-events`);
 render(tripEventsElement, createSortTemplate());
-render(tripEventsElement, createEditFormTemplate()); // не поняла нужна ли сейчас большая карточка или нет
+render(tripEventsElement, createEditFormTemplate());
 render(tripEventsElement, createCardsTemplate());
 
 const tripCardsElement = document.querySelector(`.trip-events__list`);
